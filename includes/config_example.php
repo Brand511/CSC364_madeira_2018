1a976b1825c0e78f4ea9220b8f60b32b6cd25055
Scott Madeira <scott@mygcc.org>
2018-01-29 23:23:56 -0500

    Added sample config.php file

---
+++ includes/config_example.php
@@ -0,0 +1,17 @@
+<?php
+
+if (! defined('DB_HOST')) {
+	define('DB_HOST', '127.0.0.1');
+}
+
+if (! defined('DB_DATABASE')) {
+	define('DB_DATABASE', 'csc364');
+}
+
+if (! defined('DB_USERNAME')) {
+	define('DB_USERNAME', 'username');
+}
+
+if (! defined('DB_PASSWORD')) {
+	define('DB_PASSWORD', 'password');
+}
